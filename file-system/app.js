593d32ab975c0ef5c8bba147c1d86e5f24821e81
Juan Grasso <juangrassob@gmail.com>
2023-11-30 16:31:17 -0300

    finish

--- file-system/app.js
+++ file-system/app.js
@@ -1,6 +1,11 @@
 const fs = require("fs/promises");
 
 (async () => {
+  const CREATE_FILE = "create the file";
+  const DELETE_FILE = "delete the file";
+  const RENAME_FILE = "rename the file";
+  const ADD_TO_FILE = "add to the file";
+
   const createFile = async (path) => {
     let existingFileHandler;
     try {
@@ -17,7 +22,20 @@ const fs = require("fs/promises");
     }
   };
 
-  const CREATE_FILE = "create a file";
+  const deleteFile = async (path) => {
+    await fs.unlink(path);
+    console.log("The file has been successfully deleted");
+  };
+  const renameFile = async (path, newPath) => {
+    fs.rename(path, newPath);
+    console.log("File successfully renamed:", path, " to ", newPath);
+  };
+  const addToFile = async (path, content) => {
+    const fileHandler = await fs.open(path, "a");
+    fileHandler.write(content);
+    fileHandler.close();
+    console.log("Content successfully appended");
+  };
 
   const commandFileHandler = await fs.open("./command.txt", "r");
 
@@ -41,9 +59,28 @@ const fs = require("fs/promises");
     const command = buff.toString("utf-8");
 
     if (command.includes(CREATE_FILE)) {
-      const filePath = command.split(CREATE_FILE)[1].trim();
+      const filePath = command.substring(CREATE_FILE.length + 1).trim();
 
       createFile(filePath);
+    }
+
+    if (command.includes(DELETE_FILE)) {
+      const filePath = command.substring(DELETE_FILE.length + 1).trim();
+      deleteFile(filePath);
+    }
+    if (command.includes(RENAME_FILE)) {
+      const _idx = command.indexOf("to");
+      const oldFilePath = command
+        .substring(RENAME_FILE.length + 1, _idx)
+        .trim();
+      const newFilePath = command.substring(_idx + 3).trim();
+      renameFile(oldFilePath, newFilePath);
+    }
+    if (command.includes(ADD_TO_FILE)) {
+      const _idx = command.indexOf("this content: ");
+      const filePath = command.substring(ADD_TO_FILE.length + 1, _idx).trim();
+      const content = command.substring(_idx + 14);
+      addToFile(filePath, content);
     }
   });
 

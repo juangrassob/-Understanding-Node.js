a9a3b0c69f378a6b3fa115059c52518a366ded63
Juan Grasso <juangrassob@gmail.com>
2023-12-03 17:28:58 -0300

    processing data in buffers streams

--- streams/read-big/readBig.js
+++ streams/read-big/readBig.js
@@ -7,12 +7,35 @@ const fs = require("node:fs/promises");
   const streamRead = fileHandlerRead.createReadStream();
   const streamWrite = fileHandlerWrite.createWriteStream();
 
-  streamRead.on("data", (chunk) => {
-    const availableToWrite = streamWrite.write(chunk);
+  let split = "";
 
-    if (!availableToWrite) {
-      streamRead.pause();
+  streamRead.on("data", (chunk) => {
+    const numbers = chunk.toString("utf-8").split("  ");
+
+    if (Number(numbers[0]) !== Number(numbers[1]) - 1) {
+      if (split) {
+        numbers[0] = split.trim() + numbers[0].trim();
+      }
     }
+
+    if (
+      Number(numbers[numbers.length - 1]) !==
+      Number(numbers[numbers.length - 2]) + 1
+    ) {
+      split = numbers.pop();
+    }
+
+    numbers.forEach((number) => {
+      let n = Number(number);
+
+      if (n % 2 === 0) {
+        const availableToWrite = streamWrite.write(` ${n} `, "utf-8");
+
+        if (!availableToWrite) {
+          streamRead.pause();
+        }
+      }
+    });
   });
 
   streamWrite.on("drain", () => {
